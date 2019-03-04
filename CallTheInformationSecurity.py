9dc4b8ec7a9dcb9603512b8d4b6c1f6fc0a06795
60047509 <Nikolay.Razumovskiy@leroymerlin.ru>
2019-03-04 18:53:32 +0300

    Add main function

--- CallTheInformationSecurity.py
+++ CallTheInformationSecurity.py
@@ -24,11 +24,28 @@ is_transport = [
 		"""]
 
 class ISCopter:
-    def __init__(self):
-        self.stdscr = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
-        curses.curs_set(0)
-        self.stdscr.keypad(1)
-        self.stdscr.nodelay(1)
-        self.win = curses.newwin(24, 80, 0, 0)
+	def __init__(self):
+		self.stdscr = curses.initscr()
+		curses.noecho()
+		curses.cbreak()
+		curses.curs_set(0)
+		self.stdscr.keypad(1)
+		self.stdscr.nodelay(1)
+		self.win = curses.newwin(24, 80, 0, 0)
+
+	def main(self):
+        	done = False
+        	i = 0
+        	while not(done):
+		    	self.win.addstr(0, 0, frames[i], curses.A_STANDOUT)
+		    	self.win.addstr(10, 7, 'InfoSec', curses.A_STANDOUT)
+		    	self.win.bkgd(' ', curses.A_STANDOUT)
+		    	self.win.refresh()
+			if i == 0:
+				i = 1
+			elif i == 1:
+				i = 0 
+			key = self.stdscr.getch()
+			if key == ord('q'):
+		        	done = True
+		    	sleep(0.1)
