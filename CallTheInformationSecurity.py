9558c95dd58901cfa0dda91b0f35171d21399e6f
60047509 <Nikolay.Razumovskiy@leroymerlin.ru>
2019-03-04 18:49:31 +0300

    Add informationSecurity transport

---
+++ CallTheInformationSecurity.py
@@ -0,0 +1,22 @@
+import curses
+from time import sleep
+
+is_transport = [
+		"""                     
+		 SO:SECURITY:          
+			 _^___       
+		      __/   [] \     
+		SEC===__        \    
+			\________]   
+			 I   I       
+			--------/    
+		""",
+		"""                     
+			  :SECURITY:SO 
+			 _^___       
+		 S    __/   [] \     
+		 E ===__        \    
+		 C      \________]   
+			 I   I       
+			--------/    
+		"""]
